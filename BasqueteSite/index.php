38943e57a1d42c07dc541549c5370ac34d3253be
Kauan Morais Burger <kauanburger007@gmail.com>
2024-04-09 21:31:12 -0300

    Adicionando index e css

---
+++ BasqueteSite/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela incial</title>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+    <header class="cabecalho">
+        <a href="index.php">
+            <img src="imagens/logo.png" alt="Logo da NBA" style="width:auto; height:70px; margin-top:8px;" >
+        </a>
+        <a href="Login/Login.php" class="LoginAncora">Temporada Regular</a>
+        <a href="Login/Login.php" class="LoginAncora">Playoffs</a>
+        <a href="Login/Login.php" class="LoginAncora">Estatísticas</a>
+        <a href="Login/Login.php" class="LoginAncora">Jogos</a>
+        <a href="Login/Login.php" class="LoginAncora">Times</a>
+        <a href="Login/Login.php" class="LoginAncora">Notícias</a>
+        <a href="Login/Login.php" class="LoginAncora">Login</a>
+    </header>
+</body>
+</html>
